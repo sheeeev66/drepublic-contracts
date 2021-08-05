4a52d23e1df0ef3b0a5301c395130d16e66b6007
tyler <tailinzhang1993@gmail.com>
2021-08-05 11:19:59 +0800

    refine test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ const NFTBlindBox = artifacts.require("../contracts/NFTBlindBox.sol");
 
 // https://nfts-api.origin-games.io/api/nfts/{id}
 module.exports = async (deployer) => {
-	// await deployer.deploy(DRepublic, "Tether USD", "USDT");
+	await deployer.deploy(DRepublic, "Tether USD", "USDT");
 
 	await deployer.deploy(NFTFactory, "DRepublic NFT", "DRPC", "https://drepublic.io/api/nfts/{id}");
-	// await deployer.deploy(NFTBlindBox, NFTFactory.address, DRepublic.address);
+	await deployer.deploy(NFTBlindBox, NFTFactory.address, DRepublic.address);
 };
