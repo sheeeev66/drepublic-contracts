66dff568db5a4f87fd74d4ae773b4ff00ab5052f
tyler <tailinzhang1993@gmail.com>
2021-08-08 11:54:39 +0800

    dragon incubator

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const DRepublic = artifacts.require("../contracts/DRepublic.sol");
 const NFTFactory = artifacts.require("../contracts/NFTFactory.sol");
 const NFTBlindBox = artifacts.require("../contracts/NFTBlindBox.sol");
+const NFTIncubator = artifacts.require("../contracts/NFTIncubator.sol");
 
 module.exports = async (deployer) => {
 	await deployer.deploy(DRepublic, "Tether USD", "USDT", '100000000000000000000000000', {gas: 5000000});
 	await deployer.deploy(NFTFactory, "DRepublic NFT", "DRPC", "https://drepublic.io/api/nfts/{id}");
 	await deployer.deploy(NFTBlindBox, NFTFactory.address, DRepublic.address);
+	await deployer.deploy(NFTIncubator, NFTFactory.address, NFTBlindBox.address);
 };
